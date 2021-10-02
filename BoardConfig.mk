0047caf271a8e683559abcf2173b5b825a50ec3f
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2021-10-02 16:36:12 +0200

    courbet: Enforcing SELinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,6 @@ ODM_MANIFEST_COURBET_FILES := \
 
 # Kernel
 TARGET_KERNEL_CONFIG := courbet_user_defconfig
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
